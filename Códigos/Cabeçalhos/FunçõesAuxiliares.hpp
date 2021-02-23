50ac0d45ba418faaabf32c83487ceaabf53eb40d
Gabriel Urano <gabrielurano30@gmail.com>
2021-02-23 09:28:46 -0300

    Changes

--- Códigos/Cabeçalhos/FunçõesAuxiliares.hpp
+++ Códigos/Cabeçalhos/FunçõesAuxiliares.hpp
@@ -1,6 +1,6 @@
 float calcularFuncao(float d, float a3, float a2){
     return (a3 * pow(d,3) - (9*a2*d) + 3);
-};
+}
 
 double derivadaFuncao(float d, float a3, float a2){
     double fx1 = calcularFuncao(d+1, a3, a2);
